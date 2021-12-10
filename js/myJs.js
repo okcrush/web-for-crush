02ad0562e79ae0c85159a7bdeca96fa516e84b2f
okcrush <95899051+okcrush@users.noreply.github.com>
2021-12-10 11:43:56 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "He luuuu bạn!",
+  text2: "Tui có điều này muốn hỏi bạn nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Bạn yêu tui có phải không nào >:3",
+  text4: "Nếu bạn ko trả lời mà thoát ra tức là muốn làm vợ tui rùi đó nha uwu",
+  text5: "mơ à???",
+  text6: "Yêu ơi là yêuuuuuuu <3",
+  text7: "lí do bạn thích tui đi :3",
+  text8: "Gửi cho tui <3",
+  text9: "Vì bạn đẹp tryyyy",
+  text10: "Tui biết mà ^^ Yêu bạn 3000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay tui qua đón bạn đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tui đi nàooo",
   text12: "Okii lunn <3",
 };
 
